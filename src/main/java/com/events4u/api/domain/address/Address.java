d310cc7c5eff625e5d943ce0a4b9803609abc219
Welington Carlos Alves de Almeida Filho <wcfilho98@hotmail.com>
2024-06-16 16:22:02 -0300

    feat: created entities event, coupon and addresses

---
+++ src/main/java/com/events4u/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.events4u.api.domain.address;
+
+import com.events4u.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event-id")
+    private Event event;
+}
